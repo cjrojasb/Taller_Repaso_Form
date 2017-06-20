dc89dcadccc59632942d10b0014c443a0459f515
Carlos Rojas <carlos.rojas.burgos@gmail.com>
2017-06-19 21:22:23 -0400

    Second commit - Point N°16

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,21 @@
 class PagesController < ApplicationController
   def pagina_x
+    @users = User.all
+    if params[:query].present?
+      @user = User.where("name like ? OR email like ? OR age like ?",
+      "%#{params[:query]}%", "%#{params[:query]}%", "%#{params[:query]}%")
+    else
+      @users = User.all
+    end
+  end
+
+  def new_user
+    if params[:name].present? && params[:email].present? && params[:age].present?
+      @user = User.new(name: params[:name], email: params[:email], age: params[:age])
+      @user.save
+      redirect_to pages_pagina_x_path, notice: "Usuario creado"
+    else
+      redirect_to pages_pagina_x_path, alert: "Ingrese todos los campos solicitados"
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'pages/pagina_x'
 
   root 'pages#pagina_x'
+  post 'pages/new_user'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
